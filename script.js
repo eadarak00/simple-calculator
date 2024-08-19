13b26a838230cd2d25be83008cd6099cda689f40
eadarak00 <eadarak2001@gmail.com>
2024-08-19 22:33:00 +0000

    amelioration de l'aspet visuel

--- script.js
+++ script.js
@@ -1,56 +1,122 @@
-let ecran = document.querySelector("#display")
+const ecran = document.querySelector("#display");
+let onOffButton = document.querySelector("#OnOff");
+let isOn = false;
+let valeurInitial;
 let operateur;
-let valeur;
+
+function allumerOuEteindre() {
+    isOn = !isOn;
+
+    if (isOn) {
+        ecran.value = "0";
+        valeurInitial = true;
+        onOffButton.style.backgroundColor = "#b03a2e";
+        console.log("La calculatrice est en mode ON!!");
+    } else {
+        ecran.value = "";
+        onOffButton.style.backgroundColor = "#EEE";
+        console.log("La calculatrice est en mode OFF!!");
+    }
+}
 
 function afficherSaisie(c) {
-    ecran.value += c
+    if (isOn) {
+        if (valeurInitial) {
+            ecran.value = c;
+            valeurInitial = false;
+        } else {
+            ecran.value += c;
+        }
+    }
 }
-function effacerEcran(){
-    ecran.value =""
-}
-function ajouterOperateur(op){
-    operateur = op;
-    afficherSaisie(op)
-}
-function calculerResultats(){
-    valeur = ecran.value
-    const ops = valeur.split(operateur)
-    let result;
-    let op1 = parseFloat(ops[0]);
-    let op2 = parseFloat(ops[1]);
 
-    
-    switch (operateur){
-        case "+" :
-            result = op1 + op2;
-            break;
-        case "-" : 
-            result = op1 - op2;
-            break;
-        case '*': 
-            result = op1 * op2;
-            break;
-        case '/':
-            if(op2 == 0){
-                result = "ERROR"
+function effacerEcran() {
+    if (isOn) {
+        ecran.value = "0";
+        valeurInitial = true;
+    }
+}
+
+function supprimer() {
+    if (isOn) {
+        const valeur = ecran.value;
+        ecran.value = valeur.slice(0, -1);
+    }
+}
+
+function ajouterOperateur(op) {
+    if (isOn) {
+        operateur = op;
+        afficherSaisie(op);
+    }
+}
+
+function calculerResultats() {
+    if (isOn) {
+        let result;
+
+        try {
+            const [op1, op2] = parseOperandes(ecran.value, operateur);
+            result = effectuerCalcul(op1, op2, operateur);
+            
+            if (!isFinite(result)) {
+                throw new Error("Résultat non valide");
             }
-            else{
-                result = op1 / op2;
 
-            }
-            break;
-
-    }
-
-    if(valeur  === "" || isNaN(result)){
-        result= "";
-    }
-
-    ecran.value = result
-    
-    if(result === 'ERROR'){
-        setInterval(() =>{
-            effacerEcran();
-        }, 1500)
+        } catch (error) {
+            console.error(error.message);
+            result = "ERROR";
+        } finally {
+            ecran.value = result;
+        }
     }
 }
+
+function parseOperandes(valeur, operateur) {
+    const ops = valeur.split(operateur);
+    const op1 = parseFloat(ops[0]);
+    const op2 = parseFloat(ops[1]);
+
+    if (isNaN(op1) || isNaN(op2)) {
+        throw new Error("Opérandes non valides");
+    }
+
+    return [op1, op2];
+}
+
+function effectuerCalcul(op1, op2, operateur) {
+    switch (operateur) {
+        case "+":
+            return op1 + op2;
+        case "-":
+            return op1 - op2;
+        case "*":
+            return op1 * op2;
+        case "/":
+            if (op2 === 0) {
+                throw new Error("Division par zéro");
+            }
+            return op1 / op2;
+        default:
+            throw new Error("Opérateur non supporté");
+    }
+}
+
+
+// function calculerResultats() {
+//     let result;
+    
+//     try {
+//         result = eval(ecran.value);
+        
+//         if (!isFinite(result)) {
+//             throw new Error("Résultat non valide");
+//         }
+
+//     } catch (error) {
+//         console.error(error.message);
+//         result = "ERROR";
+//     } finally {
+//         ecran.value = result;
+//     }
+// }
